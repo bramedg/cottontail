baf2709050edede914558683261c8b1c39e40a6f
bramedg <daniel.brame@gmail.com>
2025-05-01 13:58:39 -0500

    Rename tiny service to echo service and configure via environment variables

--- echoservice.js
+++ echoservice.js
@@ -1,13 +1,19 @@
+/** Author: Daniel Brame 
+ *  Echo Service is a super simple AMQP service that accepts a message on one routing key and publishes it back to another.  It assumes to always
+ *  amqp.topic
+*/
+
 const amqplib = require('amqplib');
 const EXCHANGE = 'amq.topic';
-const CONSUMER_QUEUE = "orders.get";
-const REPLY_ROUTING_KEY = "orders.get.reply";
+const CONSUMER_QUEUE = process.env['CONSUMER_QUEUE'] || 'sample_request';
+const CONSUMER_KEY = process.env["CONSUMER_KEY"] || 'sample.request';
+const REPLY_ROUTING_KEY = process.env["REPLY_KEY"] || 'sample.reply';
 
 async function main() {
     const connection = await amqplib.connect("amqp://localhost");
     const channel = await connection.createChannel();
     await channel.assertQueue(CONSUMER_QUEUE);
-    await channel.bindQueue(CONSUMER_QUEUE, EXCHANGE, CONSUMER_QUEUE);
+    await channel.bindQueue(CONSUMER_QUEUE, EXCHANGE, CONSUMER_KEY);
     await channel.consume(
         CONSUMER_QUEUE,
         (msg) => {
